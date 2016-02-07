963a7b27dddcddad0d17ab85f054f610f4b59773
Will Earp <will.earp@icloud.com>
2016-02-07 16:51:22 +0000

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description = 'Odoo XML data files library',
     author = 'Will Earp',
     author_email = 'will.earp@icloud.com',
-    url = 'https://github.com/wearp/odoo-data',
-    download_url = 'https://github.com/wearp/odoo-data/tarball/0.1',
+    url = 'https://github.com/wearp/odatoo',
+    download_url = 'https://github.com/wearp/odatoo/tarball/0.1',
     keywords = ['odoo', 'xml', 'data', 'files'],
 )
