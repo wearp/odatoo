19275826d653145df95b1233a42b0486a6e2825d
Will Earp <will.earp@icloud.com>
2016-02-17 17:24:05 +0000

    Updated to 0.3 release. 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 setup(
     name = 'odatoo',
     packages = ['odatoo', 'tests'],
-    version = '0.2',
+    version = '0.3',
     description = 'Odoo XML data files library',
     author = 'Will Earp',
     author_email = 'will.earp@icloud.com',
     url = 'https://github.com/wearp/odatoo',
-    download_url = 'https://github.com/wearp/odatoo/tarball/0.2',
+    download_url = 'https://github.com/wearp/odatoo/tarball/0.3',
     keywords = ['odoo', 'xml', 'data', 'files'],
 )
